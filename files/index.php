5cd0f371a12ae7cf001d1e8fa85607f7ec3028ba
Lane2002 <woodelaine2002@hotmail.co.uk>
2021-07-05 13:37:08 +0100

    first commit

---
+++ files/index.php
@@ -0,0 +1,47 @@
+<?php
+    // while loop: 
+    // $i = 1;
+    // while($i < 101){
+    //     if($i % 3 === 0 && $i % 5 === 0){
+    //         echo "FizzBuzz\n"; 
+    //     }elseif($i % 3 === 0){
+    //         echo "Fizz\n"; 
+    //     }elseif($i % 5 === 0){
+    //         echo "Buzz"; 
+    //     }else{
+    //         echo "$i\n";
+    //     }
+    //     $i++;
+    // }
+    
+
+//for and foreach: 
+    $output = []; 
+
+    for($i=1; $i<101; $i++){
+        if($i % 3 === 0 && $i % 5 === 0){
+            array_push($output, "FizzBuzz"); 
+        }elseif($i % 3 === 0){
+            array_push($output, "Fizz");
+        }elseif($i % 5 === 0){
+            array_push($output, "Buzz");
+        }else{
+            array_push($output, "$i");
+        }
+    }
+
+    // foreach($output as $item){
+    //     echo "$item\n";
+    // }
+
+
+    //Break and continue: 
+    foreach($output as $item){
+        if($item === "Fizz"){
+            continue;
+        }elseif($item === "FizzBuzz"){
+            echo "$item\n";
+        break;
+        }
+        echo "$item\n";
+    }
